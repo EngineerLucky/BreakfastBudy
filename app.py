8617182b59cddff61a3c65cc73ce5a0701d5736f
EngineerLucky <lucksenhlatshwayo19@gmail.com>
2024-07-23 11:43:41 -0700

    Breakfast

--- app.py
+++ app.py
@@ -1,46 +1,7 @@
-from flask_bcrypt import Bcrypt
-from flask_jwt_extended import JWTManager, create_access_token, jwt_required, get_jwt_identity
+from . import create_app
 
-# Initialize Bcrypt and JWTManager
-bcrypt = Bcrypt(app)
-app.config['JWT_SECRET_KEY'] = 'your_secret_key'
-jwt = JWTManager(app)
+app = create_app()
 
-# User Model
-class User(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    username = db.Column(db.String(80), unique=True, nullable=False)
-    password = db.Column(db.String(200), nullable=False)
-
-# Create the database
-with app.app_context():
-    db.create_all()
-
-# Register Route
-@app.route('/register', methods=['POST'])
-def register():
-    data = request.get_json()
-    hashed_password = bcrypt.generate_password_hash(data['password']).decode('utf-8')
-    new_user = User(username=data['username'], password=hashed_password)
-    db.session.add(new_user)
-    db.session.commit()
-    return jsonify({'message': 'User registered successfully'}), 201
-
-# Login Route
-@app.route('/login', methods=['POST'])
-def login():
-    data = request.get_json()
-    user = User.query.filter_by(username=data['username']).first()
-    if user and bcrypt.check_password_hash(user.password, data['password']):
-        access_token = create_access_token(identity={'username': user.username})
-        return jsonify({'access_token': access_token}), 200
-    else:
-        return jsonify({'error': 'Invalid credentials'}), 401
-
-# Protected Route Example
-@app.route('/protected', methods=['GET'])
-@jwt_required()
-def protected():
-    current_user = get_jwt_identity()
-    return jsonify({'message': f'Hello, {current_user["username"]}'}), 200
+if __name__ == '__main__':
+    app.run(debug=True)
 
