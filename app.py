8335c702bfcf19a4aa61e3156bb9140a7c8c78bd
EngineerLucky <lucksenhlatshwayo19@gmail.com>
2024-07-23 12:46:32 -0700

    Breakfast

--- app.py
+++ app.py
@@ -1,6 +1,10 @@
-from . import create_app
+from flask import Flask, render_template
 
-app = create_app()
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return render_template('index.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
